355f71403b9ffac424e2b4b1090f6c9e9bdc39a0
yukiyama <>
2020-04-10 12:34:29 +0900

    課題1,2更新
    functionの位置の修正。課題1のエラーチェックを追加。

--- js/js_kadai/area.js
+++ js/js_kadai/area.js
@@ -1,15 +1,19 @@
 //  length: 縦の長さ
 //  width: 横の長さ
-let length = 5;
-let width = 3;
+let length = 8;
+let width = 9;
 
-//  面積計算の関数呼び出し
-let area = getAria(length, width);
-console.log("面積は", area);
-
-// 周囲を求める関数の呼び出し
-let perimeter = getPerimeter(length, width);
-console.log("周囲の長さは", perimeter);
+// 長さのチェック長さが1～1000の範囲内ならture
+function checkSize(name, size) {
+  let checkSize;
+  if (size > 0 && size <= 1000) {
+    checkSize = true;
+  } else {
+    checkSize = false;
+    console.log(name, "の長さを1～1000の間で指定してください");
+  }
+  return checkSize;
+}
 
 //  四角の面積を計算する式。
 function getAria(length, width) {
@@ -23,4 +27,19 @@ function getPerimeter(length, width) {
   let perimeter;
   perimeter = (length + width) * 2;
   return perimeter;
+}
+
+// 縦横の長さが指定範囲内かチェック
+let lengthSize = checkSize("縦", length);
+let widthSize = checkSize("横", width);
+
+// サイズが指定範囲の場合、面積・周囲の長さを出す。
+if (lengthSize == true && widthSize == true) {
+  //  面積計算の関数呼び出し
+  let area = getAria(length, width);
+  console.log("面積は", area);
+
+  // 周囲を求める関数の呼び出し
+  let perimeter = getPerimeter(length, width);
+  console.log("周囲の長さは", perimeter);
 }
--- js/js_kadai/factorial.js
+++ js/js_kadai/factorial.js
@@ -1,18 +1,6 @@
 let number =　5;
 let total;
 
-// 1～10の間なら計算する、範囲外の場合エラー表示
-if (number > 0 && number <= 10) {
-
-  total = Mult(number); /*計算式呼び出し*/
-  console.log(total);
-
-} else {
-
-  console.log("1～10の間で設定してください。");
-
-}
-
 // 与えられた数字を1になるまでかけ算する
 function Mult(number) {
 
@@ -26,3 +14,16 @@ function Mult(number) {
 
   return total;　/* 結果を返す */
 }
+
+// 1～10の間なら計算する、範囲外の場合エラー表示
+if (number > 0 && number <= 10) {
+
+  total = Mult(number); /*計算式呼び出し*/
+  console.log(total);
+
+} else {
+
+  console.log("1～10の間で設定してください。");
+
+}
+
