41c15e58e2467897a4ed8a712c16ead1d1b65045
yukiyama <>
2020-04-10 12:48:14 +0900

    課題4提出
    課題4　新規作成

---
+++ js/js_kadai/even-odd.js
@@ -0,0 +1,12 @@
+function checkNumber(number) {
+  if (number % 2) {
+    console.log("奇数です");
+  } else {
+    console.log("偶数です");
+  }
+}
+
+//コンソールに「奇数です」と表示される
+checkNumber(5);
+//コンソールに「偶数です」と表示される
+checkNumber(2);
