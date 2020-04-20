0621511cd0bceb2af25e8820cdeda9aa3af8569c
yukiyama <>
2020-04-20 17:33:54 +0900

    黄金律とフィボナッチ数列を作るやつ

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -22,10 +22,23 @@ function fibonacci(number,count) {
     return arrey;
   }
   
-  
+  function fibonacci2(number,count) {
+    let arrey = [number];
+    let i = 1;
+    do {
+      arrey[i] = number;
+      number = arrey[i-1]+number;
+      i++;
+    } while (i < count);
+    return arrey;
+  }
+
 
 let arrey =  goldenRule(1597);
 console.log(arrey);
 
-let arrey2 = fibonacci(3,5); 
+let arrey2 = fibonacci(1,5); 
 console.log(arrey2);
+
+let arrey3 = fibonacci2(1,5); 
+console.log(arrey3);
